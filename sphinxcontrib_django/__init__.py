daaeaffca984567428e037c52f3c0e3c84ffb450
Diederik van der Boor <vdboor@edoburu.nl>
2018-01-02 11:30:18 +0100

    link example intersphinx_mapping to actual used versions

--- sphinxcontrib_django/__init__.py
+++ sphinxcontrib_django/__init__.py
@@ -1,4 +1,4 @@
-import sphinxcontrib_django.docstrings
+import django
 
 __version__ = "0.1"
 
@@ -18,10 +18,12 @@ def setup(app):
     sphinxcontrib_django.roles.setup(app)
 
 
-#: Example Intersphinx mapping
+#: Example Intersphinx mapping, linking to project versions
+python_version = ".".join(map(str, sys.version_info[0:2]))
+django_version = ".".join(map(str, django.VERSION[0:2]))
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3.6', None),
-    'django': ('https://docs.djangoproject.com/en/stable/', 'https://docs.djangoproject.com/en/stable/_objects/'),
+    'python': ('https://docs.python.org/' + python_version, None),
+    'django': ('https://docs.djangoproject.com/en/{}/'.format(django_version), 'https://docs.djangoproject.com/en/{}/_objects/'.format(django_version)),
     'braces': ('https://django-braces.readthedocs.org/en/latest/', None),
     'select2': ('https://django-select2.readthedocs.org/en/latest/', None),
     'celery': ('https://celery.readthedocs.org/en/latest/', None),
