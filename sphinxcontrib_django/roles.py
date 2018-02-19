b151efda7cb2353acc7d2ad71fea89696bc4b7c3
Diederik van der Boor <vdboor@edoburu.nl>
2018-02-19 10:56:28 +0100

    PEP8 fixes

--- sphinxcontrib_django/roles.py
+++ sphinxcontrib_django/roles.py
@@ -1,5 +1,6 @@
 """Adding extra roles for documentation writing."""
 
+
 def setup(app):
     """Allow this module to be used as Sphinx extension.
     This is also called from the top-level ``__init__.py``.
@@ -9,7 +10,7 @@ def setup(app):
     :type app: sphinx.application.Sphinx
     """
     app.add_crossref_type(
-        directivename = "setting",
-        rolename = "setting",
-        indextemplate = "pair: %s; setting",
+        directivename="setting",
+        rolename="setting",
+        indextemplate="pair: %s; setting",
     )
